77f64e121aed849099bcb72055890838c3e44855
vivek-1445 <vivekvora8485@gmail.com>
2021-10-23 23:14:56 +0530

    first commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class ApiController extends Controller
+{
+    public function register(){
+        dd(12);
+    }
+    public function login(){
+        dd(12);
+    }
+}
